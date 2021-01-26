b3cc141ab9e528d790ef510167f6283be9a37c6a
jeff-h <jeffvh@outlook.com>
2021-01-26 20:13:08 +1100

    add initial disclaimer component

---
+++ src/components/legal/DisclaimerScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from '../../styles/styles';
+import legalStyles from '../../styles/legalStyles';
+
+function DisclaimerScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={legalStyles.text}>DISCLAIMER: Recaura, it's heirs, assigns, representatives, employees and agents bear no responsibility and are not liable for special or consequential damages or incidents resulting from the use, misuse, inability to use or interpretation of the contents of this product regardless of whether Recaura has been advised of the possibility of such damages or incidents.</Text>
+    </View>
+  );
+}
+
+export default DisclaimerScreen;
---
+++ src/styles/legalStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+import { moderateScale } from '../utils/scaling';
+
+const legalStyles = StyleSheet.create({
+  text: {
+    marginBottom: moderateScale(20)
+  }
+});
+
+export default legalStyles;
